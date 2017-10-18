3d92c74dee970df1fee5dffe40bab41101d71901
Adrien Navratil <adrien1975@live.fr>
2017-10-18 12:15:11 +0200

    Install redirection

--- frontend/src/app/components/install/install.component.ts
+++ frontend/src/app/components/install/install.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'slark-install',
     templateUrl: 'install.component.html',
     styleUrls: ['install.component.css']
 })
-export class InstallComponent
+export class InstallComponent implements OnInit
 {
+    constructor(private router: Router)
+    {
+    }
 
+    ngOnInit()
+    {
+        this.router.navigate(['/install', {
+            outlets: {
+                'install': ['welcome']
+            }
+        }]);
+    }
 }
